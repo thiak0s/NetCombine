538a4571531376d2135044333fe3a019285579ce
thiak0s <126835181+thiak0s@users.noreply.github.com>
2024-01-23 01:56:03 -0600

    Update install_dependencies.sh
    Eliminando remanentes

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,44 +4,27 @@ echo "Este script instalará las dependencias necesarias para la herramienta Sca
 echo "Asegúrate de tener los privilegios necesarios para instalar paquetes en el sistema."
 echo "Presiona Enter para continuar o Ctrl+C para cancelar."
 read -r
-
-# Función para instalar pip y herramientas de Python
 install_python_packages() {
     apt-get install -y python3 python3-pip
     pip3 install termcolor pyfiglet platform
 }
-
-# Función para instalar herramientas mediante APT
 install_apt_packages() {
     apt-get update
     apt-get install -y nmap fierce theharvester sublist3r testssl.sh
 }
-
-# Función para descargar e instalar nikto
 install_nikto() {
     apt-get install -y nikto
 }
-
-# Función principal
 main() {
-    # Instalar herramientas mediante APT
     install_apt_packages
-
-    # Instalar pip y herramientas de Python
     install_python_packages
-
-    # Instalar nikto
     install_nikto
 
-    # Agregar directorios al PATH
     echo 'export PATH=$PATH:/usr/share/nmap/scripts' >> ~/.bashrc
     echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
 
-    # Recargar la configuración del shell
     source ~/.bashrc
 
     echo "Herramientas instaladas y configuradas correctamente."
 }
-
-# Ejecutar el script principal
 main
